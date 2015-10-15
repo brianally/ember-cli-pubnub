4fab1ec81516f9724d169f1aa6bf752021f8377f
brian ally <brian@zijn-digital.com>
2015-10-15 17:51:11 -0400

    still fiddling

--- addon/initializers/pubnub.js
+++ addon/initializers/pubnub.js
@@ -4,7 +4,7 @@ export function initialize(container, app) {
 	app.register("pubnub:main", PubNubService, {singleton: true});
 	app.inject("route", "pubnub","pubnub:main");
 	app.inject("controller", "pubnub", "pubnub:main");
-};
+}
 
 export default {
 	name      : "pubnub",
--- blueprints/ember-cli-pubnub/index.js
+++ blueprints/ember-cli-pubnub/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  description: "Provides the PubNub JavaScript SDK"
+  description: "Provides the PubNub JavaScript SDK",
 
   afterInstall: function(options) {
     return addBowerPackageToProject("pubnub");
