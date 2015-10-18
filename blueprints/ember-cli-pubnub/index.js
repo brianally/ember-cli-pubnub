e3da800375d73f3f90d9f2409a245710f99694b4
Martin Broerse <broerse@martinic.com>
2015-10-18 08:57:04 +0200

    Add normalizeEntityName to blueprint

--- blueprints/ember-cli-pubnub/index.js
+++ blueprints/ember-cli-pubnub/index.js
@@ -1,7 +1,8 @@
 module.exports = {
   description: "Provides the PubNub JavaScript SDK",
 
-  afterInstall: function(options) {
+  afterInstall: function() {
+    normalizeEntityName: function() {},
     return this.addBowerPackageToProject("pubnub", "0.0.5");
   }
 };
