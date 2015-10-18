2c2c37f55e4e6e74f677265ced2bfd901f2a8526
Martin Broerse <broerse@martinic.com>
2015-10-18 09:03:52 +0200

    Change pubnub to version 3.7.15

--- blueprints/ember-cli-pubnub/index.js
+++ blueprints/ember-cli-pubnub/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   description: "Provides the PubNub JavaScript SDK",
   normalizeEntityName: function() {},
-  
+
   afterInstall: function() {
-    return this.addBowerPackageToProject("pubnub", "0.0.5");
+    return this.addBowerPackageToProject("pubnub", "3.7.15");
   }
 };
